6b57d224071cc9e1f7ccdb59cf8b4718ffc255c4
Stefania Ionescu <test@test.com>
2021-04-08 16:26:29 +0300

    first feature2 commit

---
+++ part2.py
@@ -0,0 +1,11 @@
+print("Ai ajuns in Asia.")
+print("Ce alegi sa faci?")
+print('1. Mergi in Tokyo')
+print('2. Te intorci in Europa')
+optiune = int(input('Unde vei merge?'))
+if optiune == 1:
+    print('Te afli in Tokyo')
+elif optiune == 2:
+    print('Ai ales sa te intorci in Europa')
+else:
+    print('Optiune invalida')

0e782ea330950146b547efcd0539479d9d5d41bb
Stefi <ballo2005beatrice@gmail.comtouch main.pyvim main.pyprint(hello>
2021-04-08 17:04:16 +0300

    prima zi

---
+++ europabun.py
@@ -0,0 +1,7 @@
+oe = int(input("Ai ajuns in Europa, Ollie! Sper sa-ti placa aici. Te-ai obisnuit cu alegerile? Esti pus in fata uneia noi: mergi in Brasov (1) sau in Barcelona (2)?"))
+
+if oe == 1:
+	print ("Ai aterizat in Brasov, unul dintre cele mai frumoase orase din Romania. Aici mai ai de facut o alegere: vrei sa interactionezi cu oamenii sau vrei sa te feresti de ei?")
+else:
+	print ("Ai aterizat in Barcelona, un oras calduros nu numai la propriu, ci si la figurat. Oamenii de aici sunt foarte prietenosi. Pacat ca nu-i poti cunoaste... de fapt, e alegerea ta daca vrei sa-i cunosti sau nu. Te vezi cu oamenii (1) sau te izolezi (2)?")
+

31d0785ea3cb82cf998eb3428b17d9cbbd1667b9
stefiEU <80402268+stefiEU@users.noreply.github.com>
2021-04-09 16:45:19 +0300

    Delete inceput.py

--- inceput.py
+++
@@ -1,6 +0,0 @@
-inceput = int(input("Am o misiune importanta pentru tine, nu ma dezamagi! Esti un extraterestru numit Ollie si ai plecat spre Pamant. In parcursul tau pe Terra, vei avea de facut mai ulte decizii care iti vor influenta considerabil viitorul pe planeta dominata de oameni. Ce zici? Vrei sa intri in aceasta aventura? (1-da/0-nu)"))
-
-if inceput == 0:
-	break
-else:
-	print ("Buna alegere! Mult succes, Ollie! Acum, trebuie sa faci a doua ta alegere: mergi in Europa (1) sau in Asia (2)?")

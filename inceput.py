b8d21fa621a9b25653a912a51d3d031c48a46c83
Stefi <ballo2005beatrice@gmail.comtouch main.pyvim main.pyprint(hello>
2021-04-09 16:04:06 +0300

    primele doua

--- inceput.py
+++ inceput.py
@@ -4,3 +4,14 @@ if inceput == 0:
 	break
 else:
 	print ("Buna alegere! Mult succes, Ollie! Acum, trebuie sa faci a doua ta alegere: mergi in Europa (1) sau in Asia (2)?")
+
+
+oe = int(input("Ai ajuns in Europa, Ollie! Sper sa-ti placa aici. Te-ai obisnuit cu alegerile? Esti pus in fata uneia noi: mergi in Brasov (1) sau in Barcelona (2)?"))
+
+if oe == 1:
+	print ("Ai aterizat in Brasov, unul dintre cele mai frumoase orase din Romania. Aici mai ai de facut o alegere: vrei sa interactionezi cu oamenii sau vrei sa te feresti de ei?")
+else:
+	print ("Ai aterizat in Barcelona, un oras calduros nu numai la propriu, ci si la figurat. Oamenii de aici sunt foarte prietenosi. Pacat ca nu-i poti cunoaste... de fapt, e alegerea ta daca vrei sa-i cunosti sau nu. Te vezi cu oamenii sau te izolezi?")
+
+print('BOOM! A explodat planeta! PA!')
+        break

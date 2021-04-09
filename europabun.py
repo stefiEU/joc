c5d105f0145ce1a5dc7ab5adc331a9d46c36b67b
stefiEU <80402268+stefiEU@users.noreply.github.com>
2021-04-09 15:37:08 +0300

    Update europabun.py

--- europabun.py
+++ europabun.py
@@ -3,7 +3,7 @@ oe = int(input("Ai ajuns in Europa, Ollie! Sper sa-ti placa aici. Te-ai obisnuit
 if oe == 1:
 	print ("Ai aterizat in Brasov, unul dintre cele mai frumoase orase din Romania. Aici mai ai de facut o alegere: vrei sa interactionezi cu oamenii sau vrei sa te feresti de ei?")
 else:
-	print ("Ai aterizat in Barcelona, un oras calduros nu numai la propriu, ci si la figurat. Oamenii de aici sunt foarte prietenosi. Pacat ca nu-i poti cunoaste... de fapt, e alegerea ta daca vrei sa-i cunosti sau nu. Te vezi cu oamenii (1) sau te izolezi (2)?")
+	print ("Ai aterizat in Barcelona, un oras calduros nu numai la propriu, ci si la figurat. Oamenii de aici sunt foarte prietenosi. Pacat ca nu-i poti cunoaste... de fapt, e alegerea ta daca vrei sa-i cunosti sau nu. Te vezi cu oamenii sau te izolezi?")
 
 print('BOOM! A explodat planeta! PA!')
         break
